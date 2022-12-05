b85b6b1182cdc04922ce837ec59b4f4baf1abf9e
Bethel-Iyke <talk2bethel017@gmail.com>
2022-12-04 21:22:06 -0800

    ORM project

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+a script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+
+"""
+import sys
+import MySQLdb
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                                     passwd=sys.argv[2], port=3306, db=sys.argv[3])
+cur = db.cursor()
+cur.execute("SELECT * FROM `states` ORDER BY id ASC")
+query_rows = cur.fetchall()
+for row in query_rows:
+if row[1][0] == "N":
+print(row)
---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Displays all values in the states table of the database hbtn_0e_0_usa
+whose name matches that supplied as argument.
+
+"""
+import sys
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+        db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+            passwd=sys.argv[2], port=3306,
+            db=sys.argv[3], charset="utf8")
+cur = db.cursor()
+cur.execute("SELECT * FROM states WHERE name \
+LIKE %s ORDER BY id ASC", (argv[4],))
+rows = cur.fetchall()
+for row in rows:
+print(row)
