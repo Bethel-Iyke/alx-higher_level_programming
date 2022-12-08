2b3489e155a8af3e070a103deb46ab154ff524a6
Bethel-Iyke <talk2bethel017@gmail.com>
2022-12-07 17:19:56 -0800

    next corrected

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const count = process.argv.lenth;
-console.log(count === 2 ? 'No argument' :
+console.log(count === 0 ? 'No argument' ; count === 1? 'Argument found' ; count === 2? 'Arguments found' );
