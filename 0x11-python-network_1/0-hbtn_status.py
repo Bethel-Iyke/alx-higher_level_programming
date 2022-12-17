77d67a1787aa4cc4231a9197106085201ee41282
Bethek-Iyke <talk2bethel017@gmail.com>
2022-12-17 23:34:03 +0100

    correct

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
-        print("\t- type: {}".format(body))
+        print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode("utf-8")))
