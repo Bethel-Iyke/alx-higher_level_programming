a623cb1a306d7a99f02ea2b94656af98574dfb75
Bethek-Iyke <talk2bethel017@gmail.com>
2022-12-17 01:46:10 +0100

    python network1

---
+++ 0x10-python-network_0/0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import urllib.request
+
+if __name__ == "__main__":
+    request = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- type: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
