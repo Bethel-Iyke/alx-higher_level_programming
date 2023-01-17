8ca238b098ac5b27fc88c8cf9bfe50978f37d6c2
Bethek-Iyke <talk2bethel017@gmail.com>
2023-01-17 23:49:24 +0100

    web scrapping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
+'use strict';
 
-const fs = require('fs')
-const file = process.argv[2];
-const finput = process.argv[3];
+const fs = require('fs');
+const filePath = process.argv[2];
+const data = process.argv[3];
 
-fs.writeFile(file, finput, 'utf-8', function(err) {
-	if (err) console.log(err);
+fs.writeFile(filePath, data, 'utf8', (err) => {
+  if (err) {
+    console.log(err);
+  }
 });
